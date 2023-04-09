89cd2dcfb87310fa76e2d15db94c691ac3c1a6c7
Alvenis Becerra <Alvenisj@gmail.com>
2023-04-09 13:46:40 -0500

    dynamic responsive menu of a website, developed with vanilla javascript

---
+++ data.js
@@ -0,0 +1,30 @@
+const sublinks = [
+  {
+    page: 'productos',
+    links: [
+      { label: 'Pagar', icon: 'fas fa-credit-card', url: 'products.html' },
+      { label: 'Sulfónico', icon: 'fas fa-home-lg', url: 'products.html' },
+      { label: 'Hipoclorito', icon: 'fas fa-wine-bottle', url: 'products.html' },
+      { label: 'Mistolin', icon: 'fas fa-spray-can', url: 'products.html' }, 
+      { label: 'Herramientas', icon: 'fas fa-tools', url: 'products.html' },
+    ],
+  },
+  {
+    page: 'servicios',
+    links: [
+      { label: 'domicilios', icon: 'fas fa-truck', url: 'products.html' },
+      { label: 'Al mayor', icon: 'fas fa-dolly-flatbed', url: 'products.html' },
+      { label: 'Al detal', icon: 'fas fa-dolly', url: 'products.html' },
+      { label: '24 horas', icon: 'far fa-hourglass-half', url: 'products.html' },
+    ],
+  },
+  {
+    page: 'Empresa',
+    links: [
+      { label: 'Nosotros', icon: 'fas fa-briefcase', url: 'products.html' },
+      { label: 'clientes', icon: 'fas fa-briefcase', url: 'products.html' },
+    ],
+  },
+];
+
+export default sublinks;
